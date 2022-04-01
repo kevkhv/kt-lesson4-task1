657f0793b3692542ad7a41429bc9c3526009a9ab
kevkhv <kevkhv@yandex.ru>
2022-04-01 16:24:33 +0300

    Add fun add & update

--- src/main/kotlin/WallService.kt
+++ src/main/kotlin/WallService.kt
@@ -1,3 +1,18 @@
 object WallService {
-    var arrayPosts = emptyArray<Post>()
+   private var arrayPosts = emptyArray<Post>()
+
+    fun add(post: Post): Post {
+        arrayPosts += post.copy(id = arrayPosts.size + 1)
+        return arrayPosts.last()
+    }
+
+    fun update(post: Post): Boolean {
+        for (cell in arrayPosts) {
+            if (cell.id == post.id) {
+                arrayPosts[cell.id - 1] = post.copy(id = cell.id, ownerId = cell.ownerId, date = cell.date)
+            }
+            return true
+        }
+        return false
+    }
 }
